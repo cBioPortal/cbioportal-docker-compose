78aa11c412ad8d20f312585cb10437a86540257d
Kurt Mueller <66080261+kurt-mueller-osumc@users.noreply.github.com>
2020-09-14 12:19:34 -0400

    Use 3.4.11 docker image in init.sh

--- config/init.sh
+++ config/init.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker run --rm -it cbioportal/cbioportal:3.4.8 cat /cbioportal-webapp/WEB-INF/classes/portal.properties | \
+docker run --rm -it cbioportal/cbioportal:3.4.11 cat /cbioportal-webapp/WEB-INF/classes/portal.properties | \
     sed 's/db.host=.*/db.host=cbioportal_database:3306/g' | \
     sed 's|db.connection_string=.*|db.connection_string=jdbc:mysql://cbioportal_database:3306/|g' \
 > portal.properties
