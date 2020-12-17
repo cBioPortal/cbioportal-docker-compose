1cac913285ec7c29de187bb32cb645cc08a7503d
Ino de Bruijn <ino@ino.bio>
2020-12-17 10:08:10 -0500

    use S3 to download datahub studies

--- study/init.sh
+++ study/init.sh
@@ -7,6 +7,6 @@ DATAHUB_STUDIES="${DATAHUB_STUDIES:-lgg_ucsf_2014}"
 for study in ${DATAHUB_STUDIES}; do
     mkdir -p ${study} && \
         cd ${study} && \
-        wget -O ${study}.tar.gz "http://download.cbioportal.org/${study}.tar.gz"
+        wget -O ${study}.tar.gz "https://cbioportal-datahub.s3.amazonaws.com/${study}.tar.gz"
         tar xvfz ${study}.tar.gz
 done
