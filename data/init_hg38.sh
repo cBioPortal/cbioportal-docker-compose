ac4d64af4255578b55990de26fe08c9b327c41d3
Gaofei Zhao <15748980+dippindots@users.noreply.github.com>
2023-07-11 17:47:10 -0400

    Update init_hg38.sh to address new seed db location

--- data/init_hg38.sh
+++ data/init_hg38.sh
@@ -1,3 +1,3 @@
 # Download the seed database
 
-wget -O cgds.sql "https://raw.githubusercontent.com/cBioPortal/cbioportal/v5.3.6/db-scripts/src/main/resources/cgds.sql" && wget -O seed.sql.gz "https://github.com/cBioPortal/datahub/raw/master/seedDB_hg38/seed-cbioportal_hg19_hg38_v2.12.14.sql.gz"
+wget -O cgds.sql "https://raw.githubusercontent.com/cBioPortal/cbioportal/v5.3.6/db-scripts/src/main/resources/cgds.sql" && wget -O seed.sql.gz "https://github.com/cBioPortal/datahub/raw/master/seedDB_hg38/seedDB_hg19_hg38_archive/seed-cbioportal_hg19_hg38_v2.12.14.sql.gz"
