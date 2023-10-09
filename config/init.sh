383284d392483c15fb87f0121b9fc1e16702539b
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-10-09 16:02:32 -0400

    Fix database import issues
    The JDBCUtil class expects old props. Needs to be updated

--- config/init.sh
+++ config/init.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 VERSION=$(grep DOCKER_IMAGE_CBIOPORTAL ../.env | cut -d '=' -f 2-)
 
-docker run --rm -it $VERSION cat /cbioportal-webapp/portal.properties | \
+docker run --rm -it $VERSION cat /cbioportal-webapp/portal.properties |
+    sed 's|spring.datasource.url=.*|spring.datasource.url=jdbc:mysql://cbioportal-database:3306/cbioportal?useSSL=false\&allowPublicKeyRetrieval=true|' | \
     sed 's|spring.datasource.password=.*|spring.datasource.password=somepassword|' | \
     sed 's|spring.datasource.username=.*|spring.datasource.username=cbio_user|' | \
     sed 's|spring.datasource.url=.*|spring.datasource.url=jdbc:mysql://cbioportal-database:3306/cbioportal?useSSL=false\&allowPublicKeyRetrieval=true|' \
 > portal.properties
+
+#TODO: this is a hack to get around the JDBCUtil class not working yet with
+# spring.datasource props
+echo db.user=cbio_user >> portal.properties
+echo db.password=somepassword >> portal.properties
+echo db.driver=com.mysql.jdbc.Driver >> portal.properties
+echo 'db.connection_string=jdbc:mysql://cbioportal-database:3306/cbioportal?useSSL=false&allowPublicKeyRetrieval=true' >> portal.properties
