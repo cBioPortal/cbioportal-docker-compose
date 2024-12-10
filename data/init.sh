28bbc6337fe9db094667268ea7b0d656127aea08
Zain Nasir <49543216+zainasir@users.noreply.github.com>
2024-12-10 12:47:13 -0500

    Update init script to use latest env vars

--- data/init.sh
+++ data/init.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-VERSION=$(grep DOCKER_IMAGE_CBIOPORTAL ../.env | cut -d '=' -f 2-)
+VERSION=$(grep DOCKER_IMAGE_CBIOPORTAL ../.env | tail -n 1 | cut -d '=' -f 2-)
 
 # Get the schema
 docker run --rm -it $VERSION cat /cbioportal/db-scripts/cgds.sql > cgds.sql
