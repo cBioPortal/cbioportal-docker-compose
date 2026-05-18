bcf1dae6b30811f26bb43648794eac92f138decb
James Ko <jamesqko@gmail.com>
2026-05-18 10:53:57 -0400

    add logic to pull study_es_0 and required gene panels in study/init.sh

--- study/init.sh
+++ study/init.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
-# download data hub study and import
+set -eo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Pull study_es_0 out of the cBioPortal image
+VERSION=$(grep DOCKER_IMAGE_CBIOPORTAL "${SCRIPT_DIR}/../.env" | tail -n 1 | cut -d '=' -f 2-)
+CONTAINER=$(docker create "$VERSION")
+trap 'docker rm $CONTAINER' EXIT
+docker cp "$CONTAINER:/cbioportal/test/study_es_0" "${SCRIPT_DIR}/study_es_0"
+
+# Download datahub studies
 DATAHUB_STUDIES="${DATAHUB_STUDIES:-lgg_ucsf_2014 msk_impact_2017}"
 for study in ${DATAHUB_STUDIES}; do
-        curl -fL -o "${study}.tar.gz" "https://datahub.assets.cbioportal.org/${study}.tar.gz"
-        tar xvfz ${study}.tar.gz
+    curl -fL -o "${study}.tar.gz" "https://datahub.assets.cbioportal.org/${study}.tar.gz"
+    tar xfz "${study}.tar.gz"
+done
+
+# Collect gene panel reference data
+mkdir -p "${SCRIPT_DIR}/reference_data"
+
+# Copy test panels bundled with study_es_0
+cp "${SCRIPT_DIR}/study_es_0/data_gene_panel_testpanel1.txt" "${SCRIPT_DIR}/reference_data/"
+cp "${SCRIPT_DIR}/study_es_0/data_gene_panel_testpanel2.txt" "${SCRIPT_DIR}/reference_data/"
+
+# Download IMPACT panels from datahub
+PANEL_BASE="https://media.githubusercontent.com/media/cBioPortal/datahub/refs/heads/master/reference_data/gene_panels"
+for panel in data_gene_panel_impact341.txt data_gene_panel_impact410.txt; do
+    curl -fL -o "${SCRIPT_DIR}/reference_data/${panel}" "${PANEL_BASE}/${panel}"
 done
