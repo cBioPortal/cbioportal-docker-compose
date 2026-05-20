832a52c2dab6d730403d3c50430d0516bf5933c5
James Ko <jamesqko@gmail.com>
2026-05-20 12:14:06 -0400

    revamp study/init script to pull study_es_0 from the master branch of cbioportal

--- study/init.sh
+++ study/init.sh
@@ -3,17 +3,19 @@ set -eo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Pull study_es_0 out of the cBioPortal image
-VERSION=$(grep DOCKER_IMAGE_CBIOPORTAL "${SCRIPT_DIR}/../.env" | tail -n 1 | cut -d '=' -f 2-)
-CONTAINER=$(docker create "$VERSION")
-trap 'docker rm $CONTAINER' EXIT
-docker cp "$CONTAINER:/cbioportal/test/study_es_0" "${SCRIPT_DIR}/study_es_0"
+# Clone cbioportal repo (shallow) and pull study_es_0 and genesets from it
+CBIO_REPO="${SCRIPT_DIR}/cbioportal"
+trap 'rm -rf "$CBIO_REPO"' EXIT
+git clone --depth 1 https://github.com/cBioPortal/cbioportal.git "$CBIO_REPO"
+rm -rf "${SCRIPT_DIR}/study_es_0"
+cp -r "$CBIO_REPO/test/test_data/study_es_0" "${SCRIPT_DIR}/study_es_0"
 
 # Download datahub studies
 DATAHUB_STUDIES="${DATAHUB_STUDIES:-lgg_ucsf_2014 msk_impact_2017}"
 for study in ${DATAHUB_STUDIES}; do
     curl -fL -o "${study}.tar.gz" "https://datahub.assets.cbioportal.org/${study}.tar.gz"
     tar xfz "${study}.tar.gz"
+    rm "${study}.tar.gz"
 done
 
 # Collect gene panel reference data
@@ -29,8 +31,5 @@ for panel in data_gene_panel_impact341.txt data_gene_panel_impact410.txt; do
     curl -fL -o "${SCRIPT_DIR}/reference_data/${panel}" "${PANEL_BASE}/${panel}"
 done
 
-# Download genesets for study_es_0
-GENESET_BASE="https://raw.githubusercontent.com/cBioPortal/cbioportal-core/refs/heads/main/src/test/resources/genesets"
-for f in study_es_0_genesets.gmt study_es_0_tree.yaml study_es_0_supp-genesets.txt; do
-    curl -fL -o "${SCRIPT_DIR}/reference_data/${f}" "${GENESET_BASE}/${f}"
-done
+# Copy genesets from cloned repo
+cp "$CBIO_REPO/test/test_data/genesets/"* "${SCRIPT_DIR}/reference_data/"
